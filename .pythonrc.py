ca6c42a5201b98347a85f108f6699504b40e092b
yith <almizaar@yahoo.co.jp>
2014-04-01 23:43:17 +0900

    added .pythonrc.py

---
+++ .pythonrc.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import readline
+import rlcompleter
+if 'libedit' in readline.__doc__:
+  readline.parse_and_bind("bind ^I rl_complete")
+else:
+  readline.parse_and_bind("tab: complete")
+
+import sys
+import codecs
+reload(sys)
+sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
+sys.stdin = codecs.getreader('utf_8')(sys.stdin)
+sys.setdefaultencoding('utf-8')
+
+import re, pprint
+def pp(obj):
+  pp = pprint.PrettyPrinter(indent=4, width=160)
+  str = pp.pformat(obj)
+  return re.sub(r"\\u([0-9a-f]{4})", lambda x: unichr(int("0x"+x.group(1), 16)), str)
+
+#import nltk
+#from nltk.corpus.reader import *
+#from nltk.corpus.reader.util import *
+#from nltk.text import Text
+
